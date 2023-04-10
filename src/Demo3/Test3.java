f818fd88127d3d5308a201ea7659ccb4e787975a
19495 <19495@ALRAWI.myfiosgateway.com>
2023-04-10 12:12:09 -0400

    subrtact method

--- src/Demo3/Test3.java
+++ src/Demo3/Test3.java
@@ -4,5 +4,8 @@ public class Test3 {
 	public static void main(String[] args) {
 		System.out.println("Test3 In process...");
 	}
+	public void subtract( int a, int b) {
+		System.out.println( b - a);
+	}
 
 }
